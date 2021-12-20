27f5247ba25c63c64a2f0aa4c77cecf21445a1ad
qianxia <qianxia@summer.com>
2021-12-20 17:27:29 +0800

    修改1代码结构

--- app/user/handler.go
+++ app/user/handler.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/qianxia/blog/command"
@@ -14,18 +13,25 @@ import (
 
 // 注册
 func registerHandler(ctx *gin.Context) {
-	var err error
 	var user model.User
 	// 绑定表单数据
 	ctx.ShouldBind(&user)
 
-	Db := utils.GetDB()
-	// 判断用户名是否存在
-	if err = Db.Where("username = ?", user.Username).Find(&user).Error; err == nil {
-		// ctx.JSON(500, gin.H{"code": 500, "msg": "用户名已存在"})
-		command.Failed(ctx, http.StatusInternalServerError, 500, "用户名已存在")
+	var userService UserService
+	_, err := userService.Register(user)
+
+	if err != nil {
+		command.Failed(ctx, http.StatusInternalServerError, 500, err.Error())
 		return
 	}
+
+	// Db := utils.GetDB()
+	// // 判断用户名是否存在
+	// if err = Db.Where("username = ?", user.Username).Find(&user).Error; err == nil {
+	// 	// ctx.JSON(500, gin.H{"code": 500, "msg": "用户名已存在"})
+	// 	command.Failed(ctx, http.StatusInternalServerError, 500, "用户名已存在")
+	// 	return
+	// }
 
 	// 判断邮箱是否注册
 	// if err = Db.Where("email = ?", user.Email).Find(&user).Error; err == nil {
@@ -34,30 +40,30 @@ func registerHandler(ctx *gin.Context) {
 	// 	return
 	// }
 	// 对明文进行加密处理
-	newPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		// ctx.JSON(500, gin.H{"code": 500, "msg": "加密失败"})
-		command.Failed(ctx, http.StatusInternalServerError, 500, "加密失败")
-		return
-	}
+	// newPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	// if err != nil {
+	// 	// ctx.JSON(500, gin.H{"code": 500, "msg": "加密失败"})
+	// 	command.Failed(ctx, http.StatusInternalServerError, 500, "加密失败")
+	// 	return
+	// }
 
 	// 创建用户
-	newUser := model.User{
-		Id:         utils.NextId(),
-		Username:   user.Username,
-		Password:   string(newPassword),
-		CreateTime: time.Now(),
-		EditTime:   time.Now(),
-	}
+	// newUser := model.User{
+	// 	Id:         utils.NextId(),
+	// 	Username:   user.Username,
+	// 	Password:   string(newPassword),
+	// 	CreateTime: time.Now(),
+	// 	EditTime:   time.Now(),
+	// }
 
 	// 数据迁移
-	Db.AutoMigrate(&newUser)
+	// Db.AutoMigrate(&newUser)
 	//创建数据
-	if err = Db.Create(&newUser).Error; err != nil {
-		// ctx.JSON(500, gin.H{"code": 500, "msg": "数据创建失败"})
-		command.Failed(ctx, http.StatusInternalServerError, 500, "数据创建失败")
-		return
-	}
+	// if err = Db.Create(&newUser).Error; err != nil {
+	// 	// ctx.JSON(500, gin.H{"code": 500, "msg": "数据创建失败"})
+	// 	command.Failed(ctx, http.StatusInternalServerError, 500, "数据创建失败")
+	// 	return
+	// }
 
 	// token := utils.CreateToken(newUser.Id)
 	// ctx.JSON(200, gin.H{"code": 200, "msg": "注册成功", "token": token})
@@ -70,12 +76,19 @@ func loginHandler(ctx *gin.Context) {
 	var form model.User
 	ctx.ShouldBind(&form)
 
-	Db := utils.GetDB()
-	user := new(model.User)
+	// Db := utils.GetDB()
+	// user := new(model.User)
 
-	if err := Db.Where("username = ?", form.Username).Find(&user).Error; err != nil {
-		// ctx.JSON(500, gin.H{"code": 500, "msg": "账户不存在"})
-		command.Failed(ctx, http.StatusInternalServerError, 500, "账户不存在")
+	// if err := Db.Where("username = ?", form.Username).Find(&user).Error; err != nil {
+	// 	// ctx.JSON(500, gin.H{"code": 500, "msg": "账户不存在"})
+	// 	command.Failed(ctx, http.StatusInternalServerError, 500, "账户不存在")
+	// 	return
+	// }
+
+	var userService UserService
+	user, err := userService.Login(form)
+	if err != nil {
+		command.Failed(ctx, http.StatusInternalServerError, 500, err.Error())
 		return
 	}
 
@@ -85,6 +98,7 @@ func loginHandler(ctx *gin.Context) {
 		command.Failed(ctx, http.StatusUnauthorized, 401, "密码错误")
 		return
 	}
+
 	// 生成token
 	token := utils.CreateToken(user.Id)
 	// ctx.JSON(200, gin.H{"code": 200, "msg": "登录成功"})
--- app/user/userService.go
+++ app/user/userService.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"errors"
 	"time"
 
 	"github.com/qianxia/blog/model"
@@ -11,12 +12,23 @@ import (
 type UserService struct {
 }
 
-func (us *UserService) Register(user model.User) model.User {
+// 注册
+func (us *UserService) Register(user model.User) (model.User, error) {
+	var err error
 	Db := utils.GetDB()
 	// 判断用户名是否存在
-	Db.Where("username = ?", user.Username).Find(&user)
+	if err = Db.Where("username = ?", user.Username).Find(&user).Error; err == nil {
+		// ctx.JSON(500, gin.H{"code": 500, "msg": "用户名已存在"})
+		// command.Failed(ctx, http.StatusInternalServerError, 500, "用户名已存在")
+		return user, errors.New("用户名已存在")
+	}
 	// 对明文进行加密处理
-	newPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	newPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		// ctx.JSON(500, gin.H{"code": 500, "msg": "加密失败"})
+		// command.Failed(ctx, http.StatusInternalServerError, 500, "加密失败")
+		return user, errors.New("加密失败")
+	}
 	// 创建用户
 	newUser := model.User{
 		Id:         utils.NextId(),
@@ -29,7 +41,32 @@ func (us *UserService) Register(user model.User) model.User {
 	// 数据迁移
 	Db.AutoMigrate(&newUser)
 	//创建数据
-	Db.Create(&newUser)
+	if err = Db.Create(&newUser).Error; err != nil {
+		// ctx.JSON(500, gin.H{"code": 500, "msg": "数据创建失败"})
+		// command.Failed(ctx, http.StatusInternalServerError, 500, "数据创建失败")
+		return newUser, errors.New("数据创建失败")
+	}
 
-	return newUser
+	return newUser, err
+}
+
+// 登录
+func (us *UserService) Login(user model.User) (model.User, error) {
+	var err error
+	Db := utils.GetDB()
+	// 判断用户名是否存在
+	if err = Db.Where("username = ?", user.Username).Find(&user).Error; err != nil {
+		// ctx.JSON(500, gin.H{"code": 500, "msg": "账户不存在"})
+		// command.Failed(ctx, http.StatusInternalServerError, 500, "账户不存在")
+		return user, errors.New("账户不存在")
+	}
+
+	// 校验密码
+	// if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(user2.Password)); err != nil {
+	// 	// ctx.JSON(401, gin.H{"code": 401, "msg": "密码错误"})
+	// 	// command.Failed(ctx, http.StatusUnauthorized, 401, "密码错误")
+	// 	return user, errors.New("密码错误")
+	// }
+
+	return user, err
 }
