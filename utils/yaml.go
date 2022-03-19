1494cd98ff136b2184385110e8521e8f360e7548
qianxia639 <2274000859@qq.com>
2022-03-19 10:31:47 +0800

    简单修改

--- utils/yaml.go
+++ utils/yaml.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"io/ioutil"
+	"os"
 
 	"github.com/qianxia/blog/config"
 	"github.com/qianxia/blog/global"
@@ -9,7 +10,8 @@ import (
 )
 
 func DeCode() (config *config.Config) {
-	fileName := "./config/application.yaml"
+	dir, _ := os.Getwd()
+	fileName := dir + "/config/application.yaml"
 	yamlFile, _ := ioutil.ReadFile(fileName)
 	err := yaml.Unmarshal(yamlFile, &config)
 	if err != nil {
