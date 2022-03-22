d057b8f9d5225eb1197efd6762bed2ae95687fed
qianxia639 <2274000859@qq.com>
2022-03-22 14:49:35 +0800

    一点点

--- model/type.go
+++ model/type.go
@@ -3,6 +3,6 @@ package model
 type Type struct {
 	Id       uint16 `json:"id"`
 	TypeName string `json:"typeName" gorm:"size:10;NOT NULL;comment:分类名"`
-	Amount   uint32 `json:"amount" gorm:"comment:分类对应的博客数量"`
+	Amount   uint32 `json:"amount" gorm:"default:0;comment:分类对应的博客数量"`
 	Blogs    []Blog
 }
