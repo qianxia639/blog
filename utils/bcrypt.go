8b5427ed5c44b9967e6344662d37851ea1643f72
qianxia639 <2274000859@qq.com>
2022-03-22 14:51:06 +0800

    。。。

--- utils/bcrypt.go
+++ utils/bcrypt.go
@@ -1,16 +1,8 @@
 package utils
 
 import (
-	"crypto/md5"
-	"fmt"
-
 	"golang.org/x/crypto/bcrypt"
 )
-
-func Md5(password []byte) string {
-	hash := md5.Sum(password)
-	return fmt.Sprintf("%x", hash)
-}
 
 func Encrypt(password string) (string, error) {
 	pwd, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
